6607ba661fec764bb2e7e7465777eb714c424695
Даниил Пронин <mail@grawl.ru>
2020-02-09 17:15:18 +1000

    update gitignore

--- single/create.sh
+++ single/create.sh
@@ -26,6 +26,8 @@ mkdir -p ${INSTANCE}
 cd ${INSTANCE}
 sudo -E chown -hR ${USER} .
 
+cp ../template/gitignore/.gitignore .
+
 cp ../template/uninstall.sh .
 echo "sudo docker network rm ${INSTANCE}_external_network ${INSTANCE}_internal_network" >> ./uninstall.sh
 bash ${DIR}/create-mstdn.sh
